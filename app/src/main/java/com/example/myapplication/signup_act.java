fbdec4519411a62969804e70d22ac66ebc9d9cc7
TheKvothe <arthurbernal1997@gmail.com>
2019-03-16 11:14:58 +0100

    Añade el uso de la camara para foto perfil
    Para poder hacer las fotos he dado acceso a la camara de  nuestro dispositivo, aun falta hacer que esa foto sea circular

--- app/src/main/java/com/example/myapplication/signup_act.java
+++ app/src/main/java/com/example/myapplication/signup_act.java
@@ -1,16 +1,40 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class signup_act extends AppCompatActivity {
-
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView ImageViewProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_act);
+        Button ButtonCamera = (Button) findViewById(R.id.buttonCameraEdit);
+        ImageViewProfile = (ImageView) findViewById(R.id.imageViewProfile);
+        ButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_REQUEST){
+            Bitmap bitmapPhoto = (Bitmap)data.getExtras().get("data");
+            ImageViewProfile.setImageBitmap(bitmapPhoto);
+        }
     }
 
     public void BackEv(View view) {
