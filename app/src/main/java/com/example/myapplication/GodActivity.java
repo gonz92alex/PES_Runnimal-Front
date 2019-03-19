ce788edbe9d8b439c7ec5a9d6f818a41e7af28f2
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-03-19 23:41:23 +0100

    Eliminar clase Dummy

--- app/src/main/java/com/example/myapplication/GodActivity.java
+++ app/src/main/java/com/example/myapplication/GodActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -64,7 +65,7 @@ public class GodActivity extends AppCompatActivity implements EntrenamientoFragm
 
     @Override
     public void onListFragmentInteraction(EntrenamientoContent.EntrenamientoItem item) {
-
+        Log.d("clickTest", "onListFragmentInteraction: clicked! ");
     }
 
     /**
