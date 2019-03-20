b8be35f4db8548b05215e9a85e95a8a955509682
TheKvothe <arthurbernal1997@gmail.com>
2019-03-20 15:21:07 +0100

    Añade la barra lateral de navegacion
    Se puede navegar por los settings, friends y ranking a traves de la barra lateral y sus respectivos fragments estan creados

--- app/src/main/java/com/example/myapplication/AjustesFragment.java
+++ app/src/main/java/com/example/myapplication/AjustesFragment.java
@@ -7,12 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-
-public class MascotasFragment extends Fragment {
+public class AjustesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mascotas, null);
+        return inflater.inflate(R.layout.fragment_ajustes, container, false);
     }
 }
--- app/src/main/java/com/example/myapplication/AmigosFragment.java
+++ app/src/main/java/com/example/myapplication/AmigosFragment.java
@@ -7,12 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-
-public class MascotasFragment extends Fragment {
+public class AmigosFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mascotas, null);
+        return inflater.inflate(R.layout.fragment_amigos, container, false);
     }
 }
