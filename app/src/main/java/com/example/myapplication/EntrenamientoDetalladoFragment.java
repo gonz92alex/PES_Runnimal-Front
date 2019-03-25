595918b975cc1a4d0b9bffc6e8cf107f90a00e1e
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-03-25 16:53:08 +0100

    crear clase adaptador (creada dentro del fragmento EntrenamientoDetallado)

--- app/src/main/java/com/example/myapplication/EntrenamientoDetalladoFragment.java
+++ app/src/main/java/com/example/myapplication/EntrenamientoDetalladoFragment.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -24,6 +29,10 @@ public class EntrenamientoDetalladoFragment extends Fragment {
     private String mDetail;
 
     private TextView mContentText;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private ArrayList<String> stepsList=new ArrayList();
 
     public EntrenamientoDetalladoFragment() {
         // Required empty public constructor
@@ -63,6 +72,52 @@ public class EntrenamientoDetalladoFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_entrenamiento_detallado, container, false);
         mContentText = view.findViewById(R.id.content_tv);
         mContentText.setText(mContent);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.steps_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+
+        adapter = new StepsAdapter(stepsList,getActivity().getApplicationContext());
+        recyclerView.setAdapter(adapter);
+
         return view;
     }
+
+
+    //Clase Adaptador para mostrar los pasos en el recycler view
+    public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.StepsViewHolder> {
+
+        ArrayList<String> stepsList;
+
+        public StepsAdapter(ArrayList<String> stepsList, Context context) {
+            this.stepsList = stepsList;
+        }
+
+        @Override
+        public StepsAdapter.StepsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.step_row, parent, false);
+            return new StepsViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final StepsAdapter.StepsViewHolder holder, int position) {
+            holder.mText.setText(stepsList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return stepsList.size();
+        }
+
+        public class StepsViewHolder extends RecyclerView.ViewHolder{
+
+            protected TextView mText;
+
+            public StepsViewHolder(View itemView) {
+                super(itemView);
+                mText = (TextView) itemView.findViewById(R.id.step_tv);
+            }
+        }
+    }
 }
+
--- app/src/main/java/com/example/myapplication/EntrenamientoFragment.java
+++ app/src/main/java/com/example/myapplication/EntrenamientoFragment.java
@@ -26,9 +26,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
-
 /**
  * A fragment representing a list of Items.
  * <p/>
