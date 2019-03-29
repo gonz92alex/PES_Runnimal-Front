b73c06aee46be3633715a87d4ef94408c210ab0b
Miguel A. Carrera <miguel.carrera89@gmail.com>
2019-03-29 17:13:05 +0100

    added markers with custom ico

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,6 +24,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -58,5 +61,31 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         } catch (SecurityException e) {
             Toast.makeText(getActivity(), "Error", Toast.LENGTH_LONG).show();
         }
+
+        initMarkers(map);
+    }
+
+    private void initMarkers(GoogleMap map) {
+        Bitmap walkImage = BitmapFactory.decodeResource(getResources(), R.drawable.walk);
+        map.addMarker(new MarkerOptions() //
+                .position(new LatLng(41.3891234,2.1212519)) //
+                .title("Jardins de la Font dels Ocellet") //
+                .icon(BitmapDescriptorFactory.fromBitmap(walkImage)));
+        map.addMarker(new MarkerOptions() //
+                .position(new LatLng(41.3875943,2.1166048)) //
+                .title("Jardines del Palacio de Pedralbes") //
+                .icon(BitmapDescriptorFactory.fromBitmap(walkImage)));
+        map.addMarker(new MarkerOptions() //
+                .position(new LatLng(41.3901778,2.1155211)) //
+                .title("Jardines de William Shakespeare") //
+                .icon(BitmapDescriptorFactory.fromBitmap(walkImage)));
+        map.addMarker(new MarkerOptions() //
+                .position(new LatLng(41.3925127,2.1205195)) //
+                .title("Jardines de Villa Amelia") //
+                .icon(BitmapDescriptorFactory.fromBitmap(walkImage)));
+        map.addMarker(new MarkerOptions() //
+                .position(new LatLng(41.3843101,2.103513)) //
+                .title("Parc de Cervantes") //
+                .icon(BitmapDescriptorFactory.fromBitmap(walkImage)));
     }
 }
