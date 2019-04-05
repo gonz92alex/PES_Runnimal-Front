449c509f3def58d39238f0380a7f9c3df1c7f163
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-04-05 20:14:51 +0200

    Crear MascotaFragment y adapter

---
+++ app/src/main/java/com/runnimal/app/android/mascota/MascotaContent.java
@@ -0,0 +1,72 @@
+package com.runnimal.app.android.mascota;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class MascotaContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<MascotaItem> ITEMS = new ArrayList<MascotaItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, MascotaItem> ITEM_MAP = new HashMap<String, MascotaItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createMascotaItem(i));
+        }
+    }
+
+    private static void addItem(MascotaItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static MascotaItem createMascotaItem(int position) {
+        return new MascotaItem(String.valueOf(position), "Item " + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class MascotaItem {
+        public final String id;
+        public final String content;
+        public final String details;
+
+        public MascotaItem(String id, String content, String details) {
+            this.id = id;
+            this.content = content;
+            this.details = details;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
