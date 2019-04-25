751d9fcf81ce525d7bed301b54db6d99013e0172
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-04-25 12:45:48 +0200

    preparar fotos

--- app/src/main/java/com/runnimal/app/android/AnadirMascotaFragment.java
+++ app/src/main/java/com/runnimal/app/android/AnadirMascotaFragment.java
@@ -1,5 +1,7 @@
 package com.runnimal.app.android;
 
+import android.Manifest;
+import android.content.Intent;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -21,6 +23,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -29,10 +33,26 @@ import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -44,6 +64,11 @@ import java.io.UnsupportedEncodingException;
  * create an instance of this fragment.
  */
 public class AnadirMascotaFragment extends Fragment {
+
+    //toDO vigiliar URL
+    private String upload_URL = "http://nidorana.fib.upc.edu/api/photo/pet/emailDueño/nombrePet";
+    private RequestQueue rQueue;
+    private ArrayList<HashMap<String, String>> arraylist;
 
 
     private static final int CAMERA_REQUEST = 1888;
@@ -77,6 +102,9 @@ public class AnadirMascotaFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestMultiplePermissions();
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
         }
@@ -127,6 +155,17 @@ public class AnadirMascotaFragment extends Fragment {
             if(requestCode == CAMERA_REQUEST) {
                 Bitmap bitmapPhoto = (Bitmap) data.getExtras().get("data");
                 ImageViewProfile.setImageBitmap(bitmapPhoto);
+
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap BitmapPhoto = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), contentURI);
+                    // imageView.setImageBitmap(bitmap);
+                    uploadImage(BitmapPhoto);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Failed!", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
@@ -208,7 +247,6 @@ public class AnadirMascotaFragment extends Fragment {
                     public void onResponse(String response) {
                         progressDialog.dismiss();
                         Log.i("VOLLEY", response);
-                        //ToDo -> si la respuesta es 'OK' redirigir a pantalla de mascotas de nuevo/al perfil de la mascota?
                         success();
                     }
                 }, new Response.ErrorListener() {
@@ -255,5 +293,121 @@ public class AnadirMascotaFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+
+    private void uploadImage(final Bitmap bitmap){
+
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, upload_URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        Log.d("ressssssoo",new String(response.data));
+                        rQueue.getCache().clear();
+                        try {
+                            JSONObject jsonObject = new JSONObject(new String(response.data));
+                            Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+
+                            jsonObject.toString().replace("\\\\","");
+
+                            if (jsonObject.getString("status").equals("true")) {
+
+                                arraylist = new ArrayList<HashMap<String, String>>();
+                                JSONArray dataArray = jsonObject.getJSONArray("data");
+
+                                String url = "";
+                                for (int i = 0; i < dataArray.length(); i++) {
+                                    JSONObject dataobj = dataArray.getJSONObject(i);
+                                    url = dataobj.optString("pathToFile");
+                                }
+                                Picasso.get().load(url).into(imageView);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                // params.put("tags", "ccccc");  add string parameters
+                return params;
+            }
+
+            /*
+             *pass files using below method
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("filename", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+
+        volleyMultipartRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rQueue = Volley.newRequestQueue(getActivity());
+        rQueue.add(volleyMultipartRequest);
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+
+    private void  requestMultiplePermissions(){
+        Dexter.withActivity(getActivity())
+                .withPermissions(
+
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
     }
 }
