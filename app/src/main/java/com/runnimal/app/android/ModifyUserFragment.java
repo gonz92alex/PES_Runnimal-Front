c20c5ad07d9020ddea30b4f7ada7d5febb86ee5b
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-05-05 17:00:20 +0200

    Subir imagen en editar perfil

--- app/src/main/java/com/runnimal/app/android/ModifyUserFragment.java
+++ app/src/main/java/com/runnimal/app/android/ModifyUserFragment.java
@@ -1,8 +1,12 @@
 package com.runnimal.app.android;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -11,10 +15,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,18 +29,36 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ModifyUserFragment extends Fragment {
     TextView textViewNombre;
     TextView textViewCorreo;
     TextView textViewPassword;
 
+    //toDO vigiliar URL
+    private String upload_URL = "http://nidorana.fib.upc.edu/api/photo/users/";
+    private RequestQueue rQueue;
+    private ArrayList<HashMap<String, String>> arraylist;
 
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView ImageViewProfile;
 
     public static Fragment newInstance() {
         ModifyUserFragment fragment = new ModifyUserFragment();
@@ -47,6 +72,16 @@ public class ModifyUserFragment extends Fragment {
         textViewNombre = (TextView) view.findViewById(R.id.EditTextAlias);
         textViewNombre.setText(SingletonSession.Instance().getUsername());
 
+        ImageViewProfile = (ImageView) view.findViewById(R.id.imageViewProfile);
+        Button ButtonCamera = (Button) view.findViewById(R.id.buttonCameraEdit);
+        ButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST);
+            }
+        });
+        requestMultiplePermissions();
 
        Button saveUserButton = view.findViewById(R.id.buttonSave);
        saveUserButton.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +98,21 @@ public class ModifyUserFragment extends Fragment {
 
     }
 
-    //llamada API
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data != null){
+            if(requestCode == CAMERA_REQUEST) {
+                Bitmap bitmapPhoto = (Bitmap) data.getExtras().get("data");
+                ImageViewProfile.setImageBitmap(bitmapPhoto);
+                //toDo hacer la llamada al clickar el boton de crear
+                uploadImage(bitmapPhoto);
+            }
+        }
+    }
 
+
+    //llamada API
     private void modifier(final String nombre) throws JSONException {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue((GodActivity)getActivity());
@@ -141,7 +189,101 @@ public class ModifyUserFragment extends Fragment {
         }
     }
 
+    private void uploadImage(final Bitmap bitmap){
+        upload_URL += SingletonSession.Instance().getMail();
+        Log.d("API", upload_URL);
 
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, upload_URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        Log.d("ressssssoo",new String(response.data));
+                        rQueue.getCache().clear();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity().getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                // params.put("tags", "ccccc");  add string parameters
+                return params;
+            }
+
+            /*
+             *pass files using below method
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("filename", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+
+        volleyMultipartRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rQueue = Volley.newRequestQueue(getActivity());
+        rQueue.add(volleyMultipartRequest);
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+
+    private void  requestMultiplePermissions(){
+        Dexter.withActivity(getActivity())
+                .withPermissions(
+
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(getActivity().getApplicationContext(), "All permissions are granted by user!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // check for permanent denial of any permission
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            // show alert dialog navigating to Settings
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
 
 }
 
