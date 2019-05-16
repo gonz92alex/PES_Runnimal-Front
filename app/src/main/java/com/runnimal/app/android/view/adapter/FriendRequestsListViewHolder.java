4b0aa99c8d8bf5a30002ca5e999b8fcf83467e33
TheKvothe <arthurbernal1997@gmail.com>
2019-05-16 15:54:18 +0200

    commit preliminar

--- app/src/main/java/com/runnimal/app/android/view/adapter/FriendRequestsListViewHolder.java
+++ app/src/main/java/com/runnimal/app/android/view/adapter/FriendRequestsListViewHolder.java
@@ -2,6 +2,7 @@ package com.runnimal.app.android.view.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.runnimal.app.android.R;
@@ -25,6 +26,11 @@ public class FriendRequestsListViewHolder extends RecyclerView.ViewHolder {
     TextView name;
     @BindView(R.id.mail_user)
     TextView mail;
+    @BindView(R.id.button_aceptar)
+    Button buttonAceptar;
+    @BindView(R.id.button_rechazar)
+    Button buttonRechazar;
+
 
     public FriendRequestsListViewHolder(View itemView, FriendRequestsPresenter friendRequestsPresenter ) {
         super(itemView);
@@ -33,10 +39,17 @@ public class FriendRequestsListViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void render(OwnerViewModel user) {
+        onAcceptClick();
+        onRejectClick();
         onItemClick(user);
         ImageUtils.setImage(user.getImageUrl(), image);
         name.setText(user.getAlias());
         mail.setText(user.getEmail());
+    }
+
+    private void onAcceptClick() {
+    }
+    private void onRejectClick() {
     }
 
     private void onItemClick(final OwnerViewModel user) {
