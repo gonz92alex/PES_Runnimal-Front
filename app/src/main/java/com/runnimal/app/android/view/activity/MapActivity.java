9891ea21856e952575546ee53fe074dc6eaa1fd8
Miguel A. Carrera <macarrera@grupogodo.com>
2019-05-22 02:36:19 +0200

    add start walk/end walk button to map

--- app/src/main/java/com/runnimal/app/android/view/activity/MapActivity.java
+++ app/src/main/java/com/runnimal/app/android/view/activity/MapActivity.java
@@ -4,15 +4,19 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 
@@ -37,6 +41,7 @@ import com.runnimal.app.android.view.util.ImageUtils;
 import com.runnimal.app.android.view.viewmodel.PointViewModel;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.inject.Inject;
 
@@ -62,6 +67,8 @@ public class MapActivity extends BaseActivity implements
     RadioButton parkButton;
     @BindView(R.id.radio_button_point_type_other)
     RadioButton otherButton;
+    @BindView(R.id.button_map_walk)
+    Button walkButton;
 
     private boolean mPermissionDenied = false;
     private GoogleMap map;
@@ -94,6 +101,7 @@ public class MapActivity extends BaseActivity implements
         initializePresenter();
         initializeAdapter();
         initFilterButtons();
+        initWalkButton();
         pointsPresenter.initialize();
     }
 
@@ -176,6 +184,30 @@ public class MapActivity extends BaseActivity implements
         });
         otherButton.setOnClickListener(view -> {
             infoWindowAdapter.filter(PointType.OTHER);
+        });
+    }
+
+    private void initWalkButton() {
+        Drawable background = walkButton.getBackground();
+        ColorStateList color = walkButton.getTextColors();
+        CharSequence text = walkButton.getText();
+        AtomicBoolean buttonPressed = new AtomicBoolean(false);
+
+        walkButton.setOnClickListener(view -> {
+            if (! buttonPressed.get()) {
+                walkButton.setBackgroundResource(R.drawable.btn_light_rounded);
+                walkButton.setTextColor(Color.parseColor("#000000"));
+                walkButton.setText(R.string.map_walk_end);
+
+                buttonPressed.set(true);
+            }
+            else {
+                walkButton.setBackground(background);
+                walkButton.setTextColor(color);
+                walkButton.setText(text);
+
+                buttonPressed.set(false);
+            }
         });
     }
 
