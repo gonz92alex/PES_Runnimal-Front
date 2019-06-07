3a0a17505a50df51b16e41806a8b3a1e8d24e1a9
Miguel A. Carrera <macarrera@grupogodo.com>
2019-06-07 04:40:23 +0200

    added dates to walk info

--- app/src/main/java/com/runnimal/app/android/view/adapter/WalksListViewHolder.java
+++ app/src/main/java/com/runnimal/app/android/view/adapter/WalksListViewHolder.java
@@ -5,22 +5,29 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.runnimal.app.android.R;
-import com.runnimal.app.android.view.presenter.PetsPresenter;
 import com.runnimal.app.android.view.presenter.WalkPresenter;
-import com.runnimal.app.android.view.util.ImageUtils;
-import com.runnimal.app.android.view.viewmodel.PetViewModel;
 import com.runnimal.app.android.view.viewmodel.WalkViewModel;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 public class WalksListViewHolder extends RecyclerView.ViewHolder {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT) //
+            .withLocale(Locale.forLanguageTag("es-ES")) //
+            .withZone(ZoneId.of("Europe/Paris"));
 
     private final WalkPresenter walkPresenter;
 
     @BindView(R.id.walk_title)
     TextView title;
+    @BindView(R.id.walk_date)
+    TextView date;
     @BindView(R.id.walk_duration)
     TextView duration;
     @BindView(R.id.walk_distance)
@@ -35,6 +42,7 @@ public class WalksListViewHolder extends RecyclerView.ViewHolder {
     public void render(WalkViewModel walk) {
         onItemClick(walk);
         title.setText(walk.getTitle());
+        date.setText("From " + DATE_TIME_FORMATTER.format(walk.getStart()) + " to " + DATE_TIME_FORMATTER.format(walk.getEnd()));
         duration.setText(walk.getDuration() + " min.");
         distance.setText(walk.getDistance() + " m.");
     }
